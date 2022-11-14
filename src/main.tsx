5d637f5f80b524fc7a6943ae3581a2a935b13d07
JohnPosada <johnposada2000@gmail.com>
2022-11-14 01:06:19 -0500

    added store with authSlice and added this store to main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { AppRouter } from "./AppRouter";
 import "./index.css";
+import { store } from "./store/store";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <AppRouter />
-    </React.StrictMode>
-  </BrowserRouter>
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
