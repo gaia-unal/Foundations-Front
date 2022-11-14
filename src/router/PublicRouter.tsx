7e7dd6cc57b3c8f6482cb4aa68c177a55799be79
JohnPosada <johnposada2000@gmail.com>
2022-11-14 01:28:43 -0500

    added loadingPage when checking credentials

--- src/router/PublicRouter.tsx
+++ src/router/PublicRouter.tsx
@@ -1,13 +1,20 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import { Login } from "../auth/pages/LoginPage";
+import { LoginPage } from "../auth/pages/LoginPage";
 import { Register } from "../auth/pages/RegisterPage";
+import { useAppSelector } from "../hooks/useRedux";
+import { LoadingPage } from "../ui/pages/LoadingPage";
 
 export const PublicRouter = () => {
+  const { status } = useAppSelector((state) => state.auth);
+
+  if (status === "checking") {
+    return <LoadingPage />;
+  }
   return (
     <>
       <div className="grid h-screen place-items-center">
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<LoginPage />} />
           <Route path="/register" element={<Register />} />
           <Route path="/*" element={<Navigate to={"/"} />} />
         </Routes>
