d6c844b1be4ef3651b0c66e7f20612b8dd934d7a
JohnPosada <johnposada2000@gmail.com>
2022-11-14 01:18:57 -0500

    created loading page

---
+++ src/ui/pages/LoadingPage.tsx
@@ -0,0 +1,7 @@
+export const LoadingPage = () => {
+  return (
+    <div className="grid h-screen place-items-center">
+      <div className="animate-spin inline-block w-10 h-10 border-[3px] border-current border-t-transparent text-white rounded-full"></div>
+    </div>
+  );
+};
