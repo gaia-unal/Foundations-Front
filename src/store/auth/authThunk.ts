cb3f34a9a5055e457bac4ce107fffb1a44affe3a
JohnPosada <johnposada2000@gmail.com>
2022-11-14 01:34:17 -0500

    added thunk for auth module with the provider firebase and auth states

---
+++ src/store/auth/authThunk.ts
@@ -0,0 +1,53 @@
+import {
+  createUserEmailAndPassword,
+  loginWithEmailAndPassword,
+  singInWithGoogle,
+} from "../../firebase/auth/providers";
+import { AppDispatch } from "../store";
+import { checking, login, loginError } from "./authSlice";
+
+export const startLoginWithEmailPassword = (
+  email: string,
+  password: string
+) => {
+  return async (dispatch: AppDispatch) => {
+    dispatch(checking());
+    const res = await loginWithEmailAndPassword(email, password);
+    if (!res.error) {
+      return dispatch(login({ ...res }));
+    }
+    dispatch(loginError(res.message));
+  };
+};
+
+export const startLoginWhitGoogle = () => {
+  return async (dispatch: AppDispatch) => {
+    dispatch(checking());
+    const res = await singInWithGoogle();
+    if (!res.error) {
+      return dispatch(login({ ...res }));
+    }
+    dispatch(loginError(res.message));
+  };
+};
+
+export const startLogout = () => {
+  return (dispatch: AppDispatch) => {
+    dispatch(loginError(null));
+  };
+};
+
+export const startRegisterUser = (
+  email: string,
+  password: string,
+  displayName: string
+) => {
+  return async (dispatch: AppDispatch) => {
+    dispatch(checking());
+    const res = await createUserEmailAndPassword(email, password, displayName);
+    if (!res.error) {
+      return dispatch(login({ ...res }));
+    }
+    dispatch(loginError(res.message));
+  };
+};
