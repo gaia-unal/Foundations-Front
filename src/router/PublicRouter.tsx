3f89db547d72983514b86765e2d6c181a68d5523
JohnPosada <johnposada2000@gmail.com>
2022-11-17 23:16:21 -0500

    added recovery route

--- src/router/PublicRouter.tsx
+++ src/router/PublicRouter.tsx
@@ -1,24 +1,21 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { LoginPage } from "../auth/pages/LoginPage";
+import { RecoveryPage } from "../auth/pages/RecoveryPage";
 import { Register } from "../auth/pages/RegisterPage";
-import { useAppSelector } from "../hooks/useRedux";
-import { LoadingPage } from "../ui/pages/LoadingPage";
 
 export const PublicRouter = () => {
-  const { status } = useAppSelector((state) => state.auth);
-
-  if (status === "checking") {
-    return <LoadingPage />;
-  }
   return (
     <>
       <div className="grid h-screen place-items-center">
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/recovery" element={<RecoveryPage />} />
           <Route path="/*" element={<Navigate to={"/"} />} />
         </Routes>
       </div>
     </>
   );
 };
+
+export default PublicRouter;
