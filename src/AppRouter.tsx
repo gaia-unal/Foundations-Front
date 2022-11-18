410a3039faebb1f949c4cfee04f88eba4cebb129
JohnPosada <johnposada2000@gmail.com>
2022-11-17 23:10:31 -0500

    added public and private routes for users, and implemented lazy loading

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,13 +1,29 @@
-import React from "react";
+import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import { PublicRouter } from "./router/PublicRouter";
+import { useAppSelector } from "./hooks/useRedux";
+import { LoadingPage } from "./ui/pages/LoadingPage";
+
+const PublicRouter = lazy(() => import("./router/PublicRouter"));
+const PrivateRouter = lazy(() => import("./router/PrivateRouter"));
 
 export const AppRouter = () => {
+  const { status } = useAppSelector((state) => state.auth);
+  if (status === "checking") {
+    return <LoadingPage />;
+  }
   return (
     <>
-      <Routes>
-        <Route path="/*" element={<PublicRouter />} />
-      </Routes>
+      <Suspense fallback={<LoadingPage />}>
+        <Routes>
+          {status === "authenticated" ? (
+            <>
+              <Route path="/*" element={<PrivateRouter />} />
+            </>
+          ) : (
+            <Route path="/*" element={<PublicRouter />} />
+          )}
+        </Routes>
+      </Suspense>
     </>
   );
 };
