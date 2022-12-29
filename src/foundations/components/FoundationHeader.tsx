3e0036632cc70a4aa47a05b551d1c3dfffeff5f1
JohnPosada <johnposada2000@gmail.com>
2022-12-29 13:08:29 -0500

    added button to add new member and fixed styles

--- src/foundations/components/FoundationHeader.tsx
+++ src/foundations/components/FoundationHeader.tsx
@@ -1,31 +1,48 @@
-import { NavLink, useLocation, useParams } from "react-router-dom";
+import { Link, NavLink, useLocation, useParams } from "react-router-dom";
 
 export const FoundationHeader = () => {
   const { uid } = useParams();
+  const { pathname } = useLocation();
 
   return (
     <div>
-      <h1 className="text-4xl border-b-2 pb-2 border-black font-bold mb-3">
-        Foundation x
-      </h1>
-      <div className="flex gap-3 mt-4">
-        <NavLink
-          to={`/foundation/${uid}/about`}
-          className={({ isActive }) =>
-            `customButtomFoundation ${isActive ? "bg-gray-400" : "bg-gray-200"}`
-          }
-        >
-          Informacion
-        </NavLink>
-        <NavLink
-          to={`/foundation/${uid}/donations`}
-          className={({ isActive }) =>
-            `customButtomFoundation ${isActive ? "bg-gray-400" : "bg-gray-200"}`
-          }
-        >
-          Usuarios
-        </NavLink>
+      <div className="flex items-center justify-between border-b-2 pb-2 border-black ">
+        <h1 className="text-4xl font-bold">Foundation x</h1>
+        <div className="flex gap-2">
+          <Link
+            to={`/foundation/${uid}/addmembers`}
+            className="bg-primary text-white px-4 py-2 rounded-md"
+            aria-disabled={pathname === `/foundation/${uid}/addmembers`}
+          >
+            Agregar miembro
+          </Link>
+        </div>
       </div>
+
+      {pathname !== `/foundation/${uid}/addmembers` && (
+        <div className="flex gap-3 mt-4">
+          <NavLink
+            to={`/foundation/${uid}/about`}
+            className={({ isActive }) =>
+              `customButtomFoundation ${
+                isActive ? "bg-gray-400" : "bg-gray-200"
+              }`
+            }
+          >
+            Informacion
+          </NavLink>
+          <NavLink
+            to={`/foundation/${uid}/members`}
+            className={({ isActive }) =>
+              `customButtomFoundation ${
+                isActive ? "bg-gray-400" : "bg-gray-200"
+              }`
+            }
+          >
+            Miembros
+          </NavLink>
+        </div>
+      )}
     </div>
   );
 };
