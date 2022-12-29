66e35d096cb944a9fb86dd73f4a8fcfe37d8e2d9
JohnPosada <johnposada2000@gmail.com>
2022-12-29 14:19:26 -0500

    created foundation interface

---
+++ src/foundations/interface/foundation.ts
@@ -0,0 +1,10 @@
+export interface foundation {
+  id: number;
+  identification: string;
+  name: string;
+  address: string;
+  phone: number;
+  email: string;
+  logo: string;
+  created_at: string;
+}
