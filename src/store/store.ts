383e78265583e9bc4e97b722477e155781dedf1f
JohnPosada <johnposada2000@gmail.com>
2022-12-29 13:55:25 -0500

    added foundation and member slices to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authSlice } from "./auth/authSlice";
+import { foundationSlice } from "./fundations/foundationSlice";
+import { memberSlice } from "./member/memberSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authSlice.reducer,
+    foundation: foundationSlice.reducer,
+    members: memberSlice.reducer,
   },
 });
 
