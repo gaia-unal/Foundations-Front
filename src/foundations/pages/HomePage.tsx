6e88f8c024fc3d5b33331e40801d236d9ab67347
JohnPosada <johnposada2000@gmail.com>
2023-04-04 21:24:03 -0500

    created home page to search members in all foundations

---
+++ src/foundations/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+import { QueryForm } from "../components/QueryForm";
+
+export const HomePage = () => {
+  return (
+    <>
+      <h1 className="text-4xl mb-4 font-semibold">Consultar</h1>
+      <QueryForm />
+    </>
+  );
+};
--- src/router/PrivateRouter.tsx
+++ src/router/PrivateRouter.tsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { FoundationRoutes } from "../foundations/router/FundationRoutes";
 import { Navbar } from "../ui/components/Navbar";
 import { SideBar } from "../ui/components/SideBar";
+import { HomePage } from "../foundations/pages/HomePage";
 
 export const PrivateRouter = () => {
   return (
@@ -13,7 +14,7 @@ export const PrivateRouter = () => {
         </div>
         <div className="w-4/5 m-4 py-4 px-6 rounded-xl bg-secondary">
           <Routes>
-            <Route path="/users" element={<h1>Users</h1>} />
+            <Route path="/users" element={<HomePage />} />
             <Route path="/foundation/:uid/*" element={<FoundationRoutes />} />
             <Route path="*" element={<Navigate to="/users" />} />
           </Routes>
