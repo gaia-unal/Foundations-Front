c7453a477f0283f5ffc8cb1312b37b366049e143
JohnPosada <johnposada2000@gmail.com>
2023-04-04 21:10:04 -0500

    created custom input select component

---
+++ src/common/components/CustomSelect.tsx
@@ -0,0 +1,33 @@
+import { ErrorMessage, useField } from "formik";
+
+interface Props {
+  label: string;
+  name: string;
+  placeholder?: string;
+  [key: string]: any;
+}
+
+export const CustomSelect = ({ label, ...props }: Props) => {
+  const [field] = useField(props);
+
+  return (
+    <>
+      <label
+        className="block text-base font-medium text-label"
+        htmlFor={props.name}
+      >
+        {label}
+      </label>
+      <select
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        {...field}
+        {...props}
+      />
+      <ErrorMessage
+        name={props.name}
+        component={"span"}
+        className="text-red-700 text-sm"
+      />
+    </>
+  );
+};
