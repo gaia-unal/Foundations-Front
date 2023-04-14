5b9e8e87af50453fee3c4b1839c739301c3dbb27
JohnPosada <johnposada2000@gmail.com>
2023-04-14 17:39:00 -0500

    created form to search member for specific param

--- src/foundations/components/QueryForm.tsx
+++ src/foundations/components/QueryForm.tsx
@@ -3,29 +3,44 @@ import { AiOutlineSearch } from "react-icons/ai";
 import * as Yup from "yup";
 import { CustomSelect } from "../../common/components/CustomSelect";
 import { InputText } from "../../common/components/InputText";
+import { useLazyCustomSearchQuery } from "../../store/fundations/foundation.api";
 import { optionsCategories } from "../data/optionSelect";
+import { useAppDispatch } from "../../hooks/useRedux";
+import { setQueryMembers } from "../../store/fundations/foundationSlice";
 
 export const QueryForm = () => {
+  const [triggerSearch] = useLazyCustomSearchQuery();
+  const dispatch = useAppDispatch();
+
+  const onClickSearch = async (values: {
+    key: string;
+    category: string;
+    param: string;
+  }) => {
+    const { data: resultMembers = [] } = await triggerSearch({ ...values });
+    dispatch(setQueryMembers(resultMembers));
+  };
+
   return (
     <>
       <Formik
-        initialValues={{ query: "", category: "", subcategory: "" }}
-        onSubmit={(values) => console.log(values)}
+        initialValues={{ key: "", category: "", param: "" }}
+        onSubmit={(values) => onClickSearch(values)}
         validationSchema={Yup.object({
-          query: Yup.string()
+          key: Yup.string()
             .required("La palabra clave es obligatoria")
             .min(3, "La palabra clave debe tener al menos 3 caracteres"),
           category: Yup.string().required("La categoría es obligatoria"),
-          subcategory: Yup.string().required("El parametro es obligatorio"),
+          param: Yup.string().required("El parametro es obligatorio"),
         })}
       >
         {({ values: { category } }) => (
           <Form>
-            <div className="flex gap-2 items-center">
+            <div className="flex h-24 gap-4 items-start">
               <div className="w-1/2">
                 <InputText
-                  key={"query"}
-                  name="query"
+                  key={"key"}
+                  name="key"
                   label="Palabra clave"
                   type="text"
                 />
@@ -45,11 +60,7 @@ export const QueryForm = () => {
                 </CustomSelect>
               </div>
               <div className="w-1/5 ">
-                <CustomSelect
-                  key={"subcategory"}
-                  name="subcategory"
-                  label="Parametro"
-                >
+                <CustomSelect key={"param"} name="param" label="Parametro">
                   <option value="">Selecciona una opción</option>
                   {optionsCategories
                     .find((option) => option.value === category)
@@ -61,7 +72,7 @@ export const QueryForm = () => {
                 </CustomSelect>
               </div>
               <button
-                className="p-2 ml-6 place-self-end mb-3 overflow-hidden rounded-lg bg-gray-200 text-lg shadow hover:bg-gray-500 flex justify-center items-center"
+                className="p-2 place-self-center overflow-hidden rounded-lg bg-gray-200 text-lg shadow hover:bg-gray-500 flex justify-center items-center"
                 type="submit"
               >
                 <AiOutlineSearch className="text-2xl" />
