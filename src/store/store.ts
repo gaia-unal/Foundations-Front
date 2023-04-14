8c7c8c0818ea519828e52ca3f9a9b7a35e59625f
JohnPosada <johnposada2000@gmail.com>
2023-04-14 17:37:44 -0500

    added foundation api and deleted member slices to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,14 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authSlice } from "./auth/authSlice";
+import { foundationApi } from "./fundations/foundation.api";
 import { foundationSlice } from "./fundations/foundationSlice";
-import { memberSlice } from "./member/memberSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authSlice.reducer,
     foundation: foundationSlice.reducer,
-    members: memberSlice.reducer,
+    foundationApi: foundationApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(foundationApi.middleware),
 });
 
 export type AppDispatch = typeof store.dispatch;
