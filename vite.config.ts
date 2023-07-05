258e8a22c47b39be58ebf18173cd692dbc132355
Julián Pachón Castrillón <jupachon@unal.edu.co>
2023-07-05 17:02:16 -0500

    Added settings in order to allow vite work with docker

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 8000, // This is the port which we will use in docker
+     watch: {
+       usePolling: true
+     }
+  }
 })
