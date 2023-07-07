390ceff6c9340618d53081977924fc1e89c4adfd
Julián Pachón Castrillón <jupachon@unal.edu.co>
2023-07-06 22:09:17 -0500

    Added router basepath

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import { store } from "./store/store";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/fundacion">
         <AppRouter />
       </BrowserRouter>
     </Provider>
